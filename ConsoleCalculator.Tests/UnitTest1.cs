707426d3e1cb1f9999de6f3b0a9745e169cc7f14
asher <ashtonday09@gmail.com>
2024-02-16 12:27:49 -0600

    Inital Commit

---
+++ ConsoleCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace ConsoleCalculator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var calculator  = new Calculator();
+        }
+    }
+}
