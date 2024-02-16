aedbdc02937a3d57ceec7641d2d83701ea68f99d
asher <ashtonday09@gmail.com>
2024-02-16 12:36:40 -0600

    Added First Unit Test

--- ConsoleCalculator.Tests/UnitTest1.cs
+++ ConsoleCalculator.Tests/UnitTest1.cs
@@ -1,12 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleCalculator;
+
 namespace ConsoleCalculator.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [TestMethod()]
         public void AddTest()
         {
-            var calculator  = new Calculator();
+            var Calc = new Calculator(1, 1);
+            
+
+            var result = Calc.Add();
+
+            Assert.IsNotNull(Calc);
+            Assert.AreEqual(2, result);
+            StringAssert.Contains(result.ToString(), "2");
         }
     }
 }
